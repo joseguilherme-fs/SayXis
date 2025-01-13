c889bae13453fa01a94c550262cecca31307fb0e
Lucas <lucasemiliano619@gmail.com>
2025-01-13 18:37:12 -0300

    Criando métodos no LikeService
    Criação dos métodos "countLikes" e "isLiked" no "LikeService".

--- src/main/java/br/edu/ifpb/pweb2/sayxis/service/LikeService.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/service/LikeService.java
@@ -1,18 +1,22 @@
 package br.edu.ifpb.pweb2.sayxis.service;
 
 import br.edu.ifpb.pweb2.sayxis.model.Like;
+import br.edu.ifpb.pweb2.sayxis.model.LikeId;
 import br.edu.ifpb.pweb2.sayxis.repository.LikeRepository;
-import br.edu.ifpb.pweb2.sayxis.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
-public class LikeService implements Service<Like, Integer>{
+public class LikeService implements Service<Like, LikeId>{
 
     @Autowired
     private LikeRepository likeRepository;
+
+    @Autowired
+    private PhotoService photoService;
 
     @Override
     public List<Like> findAll() {
@@ -20,12 +24,34 @@ public class LikeService implements Service<Like, Integer>{
     }
 
     @Override
-    public Like findById(Integer integer) {
-        return null;
+    public Like findById(LikeId likeId) {
+        Like like = null;
+        Optional<Like> likeOptional = likeRepository.findById(likeId);
+        if(likeOptional.isPresent()){
+            like = likeOptional.get();
+        }
+        return like;
     }
 
     @Override
     public Like save(Like like) {
         return likeRepository.save(like);
     }
+
+    public Integer countLikes(Integer photoId) {
+        if (photoService.findById(photoId) != null) {
+            return likeRepository.countLikes(photoId);
+        } else {
+            return null;
+        }
+    }
+
+    public Boolean isLiked(Integer photographerId, Integer photoId) {
+        if (photoService.findById(photoId) != null) {
+            return likeRepository.isLiked(photographerId, photoId);
+        } else {
+            return null;
+        }
+    }
+
 }
