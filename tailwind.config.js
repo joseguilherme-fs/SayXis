ce601af0ed8b3412568eb7bb7f7232d6c000a6fc
Sofia Leal <sofialealoliveira@gmail.com>
2025-01-18 11:55:50 -0300

    Alteração na dependência do Lombok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/templates/**/*.html"],
+  content: ['./src/main/resources/templates/*.html'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
