4456956687627f6fbb2b70a5d0fa6a021bbdff79
Mellanie Marques <103280572+Mellanie-Marques@users.noreply.github.com>
2025-01-20 20:49:51 -0300

    adicionando o requisito UC01
    adicionei o método cadastrar ao photograperController e o formulário de cadastro

--- src/main/java/br/edu/ifpb/pweb2/sayxis/controller/PhotographerController.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/controller/PhotographerController.java
@@ -2,13 +2,20 @@ package br.edu.ifpb.pweb2.sayxis.controller;
 
 import br.edu.ifpb.pweb2.sayxis.model.Photographer;
 import br.edu.ifpb.pweb2.sayxis.model.PhotographerDTO;
+import br.edu.ifpb.pweb2.sayxis.repository.PhotographerRepository;
 import br.edu.ifpb.pweb2.sayxis.service.PhotographerService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+
 
 @Controller
 @RequestMapping("/photographer")
@@ -16,6 +23,53 @@ public class PhotographerController {
 
     @Autowired
     PhotographerService photographerService;
+
+     @Autowired
+    private PhotographerRepository photographerRepository;
+
+    @GetMapping("/cadastro")
+    public ModelAndView getForm(Photographer photographer, ModelAndView model) {
+        model.addObject("photographer", photographer);
+        model.setViewName("/cadastro-form");
+        return model;
+    }
+
+    @PostMapping("/cadastro")
+    public ModelAndView cadastrar(@Valid Photographer photographer, BindingResult validation, ModelAndView model,
+                                              RedirectAttributes redAttrs) {
+        try {
+            // Verificação de erros de validação
+            if (validation.hasErrors()) {
+                model.setViewName("/cadastro-form");
+                model.addObject("photographer", photographer);
+                return model;
+            }
+
+            // Configuração de campos opcionais, caso não estejam preenchidos
+            if (photographer.getCity() == null) {
+                photographer.setCity("Cidade não informada");
+            }
+            if (photographer.getCountry() == null) {
+                photographer.setCountry("País não informado");
+            }
+
+            // Salvando o fotógrafo
+            photographerRepository.save(photographer);
+
+            // Redirecionamento e mensagem de sucesso
+            redAttrs.addFlashAttribute("mensagem", "Fotógrafo cadastrado com sucesso!");
+            model.setViewName("redirect:/cadastro-form");
+        } catch (IllegalArgumentException e) {
+            model.setViewName("/cadastro-form");
+            model.addObject("mensagem", e.getMessage());
+        } catch (Exception e) {
+            model.setViewName("/cadastro-form");
+            model.addObject("mensagem", "Erro ao cadastrar fotógrafo: " + e.getMessage());
+        }
+        return model;
+    }
+
+
 
     @GetMapping("/login")
     public String  Page(Model model, @ModelAttribute("photographerData") PhotographerDTO photographerData) {
@@ -54,3 +108,4 @@ public class PhotographerController {
         return (Integer) session.getAttribute("user_id");
     }
 }
+
