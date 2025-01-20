f58bf5ae3d28b1c32b185b07fc28a14df7a6c0cb
Lucas <lucasemiliano619@gmail.com>
2025-01-20 16:50:41 -0300

    Edita o findById no PhotoService
    Reescrevendo o método findById no PhotoService.

--- src/main/java/br/edu/ifpb/pweb2/sayxis/service/PhotoService.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/service/PhotoService.java
@@ -1,11 +1,13 @@
 package br.edu.ifpb.pweb2.sayxis.service;
 
 import br.edu.ifpb.pweb2.sayxis.model.Photo;
+import br.edu.ifpb.pweb2.sayxis.model.Photo;
 import br.edu.ifpb.pweb2.sayxis.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PhotoService implements Service<Photo, Integer>{
@@ -19,9 +21,13 @@ public class PhotoService implements Service<Photo, Integer>{
     }
 
     @Override
-    public Photo findById(Integer integer) {
-        return null;
-    }
+    public Photo findById(Integer photo_id) {
+        Photo photo = null;
+        Optional<Photo> opPhoto = photoRepository.findById(photo_id);
+        if (opPhoto.isPresent()) {
+            photo = opPhoto.get();
+        }
+        return photo;    }
 
     @Override
     public Photo save(Photo photo) {
