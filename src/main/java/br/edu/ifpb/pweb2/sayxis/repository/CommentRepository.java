29c1cf34dc9ec808e26453160e9731939f5c99c8
Lucas <lucasemiliano619@gmail.com>
2025-01-21 00:00:54 -0300

    Altera o CommentRepository e LikeRepository
    Queries dos repositórios passam a comparar atributos dos métodos com objetos.

--- src/main/java/br/edu/ifpb/pweb2/sayxis/repository/CommentRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.pweb2.sayxis.repository;
 
 import br.edu.ifpb.pweb2.sayxis.model.Comment;
+import br.edu.ifpb.pweb2.sayxis.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-    @Query("select c from Comment c where c.photo = :photo_id")
-    List<Comment> getComments(@Param("photo_id") Integer photo_id);
+    @Query("select c from Comment c where c.photo = :photo")
+    List<Comment> getComments(@Param("photo") Photo photo);
 }
--- src/main/java/br/edu/ifpb/pweb2/sayxis/repository/LikeRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/repository/LikeRepository.java
@@ -2,6 +2,8 @@ package br.edu.ifpb.pweb2.sayxis.repository;
 
 import br.edu.ifpb.pweb2.sayxis.model.Like;
 import br.edu.ifpb.pweb2.sayxis.model.LikeId;
+import br.edu.ifpb.pweb2.sayxis.model.Photo;
+import br.edu.ifpb.pweb2.sayxis.model.Photographer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,9 +12,9 @@ import org.springframework.data.repository.query.Param;
 @Repository
 public interface LikeRepository extends JpaRepository<Like, LikeId> {
 
-    @Query("select count(l) from Like l where l.photo = :photo_id")
-    int countLikes(@Param("photo_id") Integer photo_id);
+    @Query("select count(l) from Like l where l.photo = :photo")
+    int countLikes(@Param("photo") Photo photo);
 
-    @Query("select count(l) > 0 from Like l where l.photographer = :photographer_id and l.photo = :photo_id")
-    boolean isLiked(@Param("photographer_id") Integer photographer_id, @Param("photo_id") Integer photo_id);
+    @Query("select count(l) > 0 from Like l where l.photographer = :photographer and l.photo = :photo")
+    boolean isLiked(@Param("photographer") Photographer photographer, @Param("photo") Photo photo);
 }
