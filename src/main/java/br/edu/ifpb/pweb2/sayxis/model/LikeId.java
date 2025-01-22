072dc5f64202917c3db9cae01d4034acd5049f55
Lucas <lucasemiliano619@gmail.com>
2025-01-21 21:56:04 -0300

    Ajusta importações do LikeId

--- src/main/java/br/edu/ifpb/pweb2/sayxis/model/LikeId.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/model/LikeId.java
@@ -1,15 +1,12 @@
 package br.edu.ifpb.pweb2.sayxis.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
-import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
 @Data
 @Embeddable
-@Component
 @NoArgsConstructor
 @AllArgsConstructor
 
