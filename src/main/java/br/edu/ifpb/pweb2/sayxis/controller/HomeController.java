24e9f45f4ee48641243962e0e003fe8c107f44f4
Mellanie Marques <103280572+Mellanie-Marques@users.noreply.github.com>
2025-03-11 23:59:43 -0300

    modificando o controller home para receber paginação

--- src/main/java/br/edu/ifpb/pweb2/sayxis/controller/HomeController.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/controller/HomeController.java
@@ -3,9 +3,15 @@ package br.edu.ifpb.pweb2.sayxis.controller;
 import br.edu.ifpb.pweb2.sayxis.model.Photo;
 import br.edu.ifpb.pweb2.sayxis.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,9 +23,20 @@ public class HomeController {
     private PhotoService photoService;
 
     @GetMapping("/")
-    public String showHomePage(Model model) {
-        List<Photo> photos = photoService.findAll();
-        model.addAttribute("photos", photos);
+    public String showHomePage(Model model,
+                               @RequestParam(defaultValue = "0")int page,
+                               @RequestParam(defaultValue = "10")int size) {
+        Pageable pageable = PageRequest.of(page,size);
+
+        //busca as fotos paginadas
+        Page<Photo> photosPage = photoService.findAll(pageable);
+
+        // Adiciona os atributos ao modelo
+        model.addAttribute("photos", photosPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", photosPage.getTotalPages());
+        model.addAttribute("totalItems", photosPage.getTotalElements());
+
         return "index";
     }
 }
