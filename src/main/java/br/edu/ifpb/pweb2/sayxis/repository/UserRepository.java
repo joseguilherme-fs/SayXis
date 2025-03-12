47626376bfd3fc52dfc5337b80bb4f66fa25fc2b
José Guilherme <jguilhermef.s2@gmail.com>
2025-03-12 13:57:01 -0300

    Criação do UserRepository
    Criando o UserRepository para manipular corretamente a tabela Users.

---
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package br.edu.ifpb.pweb2.sayxis.repository;
+
+import br.edu.ifpb.pweb2.sayxis.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+}
+
+
