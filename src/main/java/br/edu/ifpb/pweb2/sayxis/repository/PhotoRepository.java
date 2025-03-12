c86f478419f6205f791a9db7f5cbf4cf22368c48
Mellanie Marques <103280572+Mellanie-Marques@users.noreply.github.com>
2025-03-12 00:00:27 -0300

    modificando o photo repository para receber paginação

--- src/main/java/br/edu/ifpb/pweb2/sayxis/repository/PhotoRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/sayxis/repository/PhotoRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +16,7 @@ import java.util.Optional;
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
 
     @Query("select p from Photo p where p.is_profilePhoto = false")
-    List<Photo> findAllFeed();
+    Page<Photo> findAllFeed(Pageable pageable);
 
     Optional<Photo> findById(Integer id);
 
@@ -26,4 +28,8 @@ public interface PhotoRepository extends JpaRepository<Photo, Integer> {
 
     @Query("SELECT p FROM Photo p WHERE p.photographer.id = :photographerId AND p.is_profilePhoto = false")
     List<Photo> findPhotosByPhotographer(@Param("photographerId") Integer photographerId);
+
+    // Método para buscar fotos ordenadas pela data de criação
+    @Query("SELECT p FROM Photo p ORDER BY p.createdAt DESC")
+    Page<Photo> findAllOrderByCreatedAtDesc(Pageable pageable);
 }
